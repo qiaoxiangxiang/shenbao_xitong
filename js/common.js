8dc0356be44148639be7762a66bbf40c837b1d9f
yaqoo <qiaoxiangxiang0123@163.com>
2018-09-14 19:03:09 +0800

    123

---
+++ js/common.js
@@ -0,0 +1,138 @@
+$(".top1").append(`
+<div class="top1_box">
+            <div class="top1_div1">
+                <div class="top1_div1_left">
+                    <img class="top1_div1_left_img" src="../img/index/log_03.png" alt="">
+                    <span class="top1_div1_left_txt">
+                        教材申报系统
+                    </span>
+                </div>
+                <div class="top1_div1_right">
+                    <div class="top1_div1_right_top">
+                        <a href="javascript:;"><span class="top1_denglu">专家端</span></a>
+                        <span class="top1_gexian">|</span>
+                        <a href="javascript:;"><span class="top1_zhuce">院校端</span></a>
+                    </div>
+                    <div class="top1_div1_right_bottom">
+                        <div class="top1_div1_inp">
+                            <span class="iconfont icon_sousuo">&#xe680;</span>
+                            <input class="top1_input" type="text" placeholder="请输入教材关键词">
+                        </div>
+                        <span class="top1_div1_sousuo">搜索</span>
+                    </div>
+                </div>
+            </div>
+            <div class="top1_div2">
+                <div class="top1_div2_box">
+                    <ul class="top1_div2_ul">
+                        <a class="top1_div2_nav" href="javascript:;"><li>系统首页</li></a>
+                        <a class="top1_div2_nav" href="javascript:;"><li>教材申报</li></a>
+                        <a class="top1_div2_nav" href="javascript:;"><li>我的消息</li></a>
+                        <a class="top1_div2_nav" href="javascript:;"><li>个人中心</li></a>
+                    </ul>
+                    <div class="top1_div2_right">
+                        <a href="javascript:;"><span class="top1_denglu">登录</span></a>
+                        <span class="top1_gexian">|</span>
+                        <a href="javascript:;"><span class="top1_zhuce">注册</span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+$(".top2").append(
+    `
+    <div class="top2_box">
+            <div class="top1_div1">
+                <div class="top1_div1_left">
+                    <img class="top1_div1_left_img" src="../img/index/log_03.png" alt="">
+                    <span class="top1_div1_left_txt">
+                        教材申报系统
+                    </span>
+                </div>
+                <div class="top1_div1_right">
+                    <div class="top1_div1_right_top">
+                        <div class="top1_div1_right_left">
+                            <a href="javascript:;"><span class="top1_denglu">专家端</span></a>
+                            <span class="top1_gexian">|</span>
+                            <a href="javascript:;"><span class="top1_zhuce">院校端</span></a>
+                        </div>
+                        <div class="top1_div1_right_right">
+                            <a href="javascript:;">
+                                <span class="iconfont icon_guanbi">&#xe602;</span>
+                                <span class="top1_denglu">
+                                    退出登录
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="top1_div1_right_bottom">
+                        <div class="top1_div1_inp">
+                            <span class="iconfont icon_sousuo">&#xe680;</span>
+                            <input class="top1_input" type="text" placeholder="请输入教材关键词">
+                        </div>
+                        <span class="top1_div1_sousuo">搜索</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `)
+    $(".content2_box_top").append(`
+    <div class="content2_box_top_div">
+    <div class="content2_tou_image">
+        <img class="content2_tou_img" src="../img/index/yonghu_img_04.png" alt="">
+    </div>
+    <div class="content2_tou_text">
+        <p class="yonghu1">
+            <span class="yonghu_name">张国庆</span>
+            <a class="iconfont icon_xiaoxi">
+                    &#xe629;
+                <span class="xiaoxi_num">10</span>
+            </a>
+            <a class="iconfont icon_bianxie">&#xe640;</a>
+        </p>
+        <p class="yonghu2">
+            <span class="yonghu2_xingbie">性别： 男</span>
+            <span>手机：17677839382</span>
+        </p>
+        <p class="yonghu3">
+            <span class="yonghu3_yuanxiao">所属院校：中国医药科技大学</span>
+            <span>所属职位：主编/暂无职位</span>
+        </p>
+    </div>
+</div>
+<div class="content2_box_top_div">
+    <a class="content2_tou_list">
+        <div class="content2_tou_list_image">
+            <img src="../img/index/yonghu_img_07.png" alt="">
+        </div>
+        <p class="content2_tou_list_txt">
+            我的申报
+        </p>
+    </a>
+    <a class="content2_tou_list">
+        <div class="content2_tou_list_image">
+            <img src="../img/index/yonghu_img_09.png" alt="">
+        </div>
+        <p class="content2_tou_list_txt">
+            申报进度
+        </p>
+    </a>
+    <a class="content2_tou_list">
+        <div class="content2_tou_list_image">
+            <img src="../img/index/yonghu_img_11.png" alt="">
+        </div>
+        <p class="content2_tou_list_txt">
+            选择团队
+        </p>
+    </a>
+    <a class="content2_tou_list">
+        <div class="content2_tou_list_image">
+            <img src="../img/index/yonghu_img_13.png" alt="">
+        </div>
+        <p class="content2_tou_list_txt">
+            自主上报
+        </p>
+    </a>
+</div>
+    `)
+$(".bottom").append("中国中医药出版社 版权所有 京ICP备12043164号-6 Copyright 2017 CPTCM Co.,Ltd All Rights Reserved.")
